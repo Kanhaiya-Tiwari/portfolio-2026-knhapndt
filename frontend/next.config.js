2384161327361af4e871c59e8de1e779da5d2bf7
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-03-12 03:12:24 +0530

    added github action pages workflow for deploy this portfolio

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio-2026-knhapndt',  // GitHub Pages repo path
   trailingSlash: true,
   images: {
     unoptimized: true,
